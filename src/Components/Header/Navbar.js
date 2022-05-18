21ac1bf82c81570567e8d65da585f6065aaddfa2
kmrajibfaysal <kmrajibfaysal@gmail.com>
2022-05-19 02:16:03 +0600

    added user pic showing and log out function in nav

--- src/Components/Header/Navbar.js
+++ src/Components/Header/Navbar.js
@@ -2,9 +2,15 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/no-noninteractive-tabindex */
 /* eslint-disable jsx-a11y/alt-text */
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 function Navbar() {
+    const [user] = useAuthState(auth);
+    const handleLogOut = () => signOut(auth);
+
     return (
         <div className=" bg-pink-100">
             <div className="container navbar mx-auto">
@@ -18,7 +24,13 @@ function Navbar() {
                     <div className="dropdown-end dropdown">
                         <label tabIndex="0" className="avatar btn btn-ghost btn-circle">
                             <div className="w-10 rounded-full">
-                                <img src="http://www.gravatar.com/avatar/3b3be63a4c2a439b013787725dfce802?d=identicon" />
+                                <img
+                                    src={
+                                        user?.photoURL
+                                            ? user.photoURL
+                                            : `http://www.gravatar.com/avatar/3b3be63a4c2a439b013787725dfce802?d=identicon`
+                                    }
+                                />
                             </div>
                         </label>
                         <ul
@@ -35,7 +47,9 @@ function Navbar() {
                                 <a>Settings</a>
                             </li>
                             <li>
-                                <a>Logout</a>
+                                <button onClick={handleLogOut} type="button">
+                                    Logout
+                                </button>
                             </li>
                         </ul>
                     </div>
